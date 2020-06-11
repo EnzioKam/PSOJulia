822fba22ca8774e4773569964b6e8447715148c6
EnzioKam <enziokamhh@gmail.com>
2020-06-11 23:19:25 +0800

    Fix schaffer2, schaffer4 wrong coefficient

--- src/functions/schaffer2.jl
+++ src/functions/schaffer2.jl
@@ -3,7 +3,7 @@
 
 Evaluates the schaffer2 function on the vector x of length 2 and returns the value.
 ```math
-f(x) = 0.5 + \\frac{\\sin^2(x_1^2 - x_2^2) - 0.5}{(1 + 0.0001(x_1^2 + x_2^2))^2}
+f(x) = 0.5 + \\frac{\\sin^2(x_1^2 - x_2^2) - 0.5}{(1 + 0.001(x_1^2 + x_2^2))^2}
 ```
 
 The global minimum ``f(x) = 0`` is attained at ``x = (0, 0)``. The
@@ -12,11 +12,11 @@ recommended bounds are ``-100 \\leq x_i \\leq 100``.
 # Examples
 ```jldoctest
 julia> schaffer2([1.0; 2.0])
-0.020394581994177263
+0.02467994027357423
 ```
 """
 function schaffer2(x::Vector{Float64})
     @assert length(x) == 2 "schaffer2 can only take in Vector{Float64} of length 2."
     x1, x2 = x
-    return 0.5 + (sin(x1^2 - x2^2)^2 - 0.5) / (1 + 0.0001 * (x1^2 + x2^2))^2
+    return 0.5 + (sin(x1^2 - x2^2)^2 - 0.5) / (1 + 0.001 * (x1^2 + x2^2))^2
 end
